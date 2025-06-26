2b0cf66ca3b670381008b3aad7c0ec7a010c0e4e
Wenbao Xu <mail@xuwenbao.me>
2025-06-26 13:46:14 +0800

    delete export mem memory

--- mindmatrix/__init__.py
+++ mindmatrix/__init__.py
@@ -1,7 +1,7 @@
 from .web import AgentProvider
 from ._mindmatrix import MindMatrix
 from .utils.http_client import AsyncHttpClient, SyncHttpClient
-from .agent_base import BaseAgent, BaseWorkflow, CustomRunResponse, Artifact, ZhipuAI, OpenAILike, Mem0Memory
+from .agent_base import BaseAgent, BaseWorkflow, CustomRunResponse, Artifact, ZhipuAI, OpenAILike
 from .knowledge_base import Milvus, OpenAIEmbedder, Document, VectorDbProvider
 
 __all__ = [
@@ -12,7 +12,6 @@ __all__ = [
     "Artifact",
     "ZhipuAI",
     "OpenAILike",
-    "Mem0Memory",
     "AsyncHttpClient",
     "SyncHttpClient",
     "Document",
--- mindmatrix/agent_base/__init__.py
+++ mindmatrix/agent_base/__init__.py
@@ -1,5 +1,4 @@
-from ._memory import Mem0Memory
 from ._models import ZhipuAI, OpenAILike
 from ._base import BaseAgent, BaseWorkflow, CustomRunResponse, Artifact
 
-__all__ = ["BaseAgent", "BaseWorkflow", "ZhipuAI", "OpenAILike", "CustomRunResponse", "Artifact", "Mem0Memory"]
+__all__ = ["BaseAgent", "BaseWorkflow", "ZhipuAI", "OpenAILike", "CustomRunResponse", "Artifact"]
